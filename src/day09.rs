d36b82b5e305162538eb4f3e536bff6f0c18742e
Niklas Hallqvist <niklas@zynapp.com>
2020-12-09 17:00:46 +0100

    Improve code.  Use scan instead of mapping over the inner range.

--- src/day09.rs
+++ src/day09.rs
@@ -1,3 +1,4 @@
+use std::cmp::*;
 use crate::day::*;
 
 pub struct Day09 {}
@@ -15,24 +16,34 @@ impl Day for Day09 {
 }
 
 impl Day09 {
-    fn numbers(self: &Self, input: &mut dyn io::Read) -> Vec<i64> {
-        Utils::numbers(input).map(Result::unwrap).collect::<Vec<_>>()
+    fn numbers(self: &Self, input: &mut dyn io::Read) -> BoxResult<Vec<i64>> {
+        Utils::numbers(input).collect::<BoxResult<Vec<_>>>()
     }
 
-    fn find_invalid<'a>(self: &Self, numbers: &Vec<i64>, p: usize) -> BoxResult<i64> {
+    fn find_invalid(self: &Self, numbers: &Vec<i64>, p: usize) -> BoxResult<i64> {
         Ok(numbers.windows(p + 1).map(|w| {
-            let n = w.get(p).unwrap();
-            if (&w[0..p]).iter().combinations(2).map(|c| c[0] + c[1]).find(|&s| s == *n).is_some() { None } else { Some(*n) }
+            let n = *w.last().unwrap();
+            if (&w[0..p]).iter().combinations(2).map(|c| c[0] + c[1]).find(|&s| s == n).is_some() { None } else { Some(n) }
         }).flatten().next().unwrap())
     }
 
     fn part1_impl(self: &Self, input: &mut dyn io::Read, p: usize) -> BoxResult<i64> {
-        let numbers = self.numbers(input);
-        self.find_invalid(&numbers, p)
+        self.find_invalid(&self.numbers(input)?, p)
     }
 
     fn part2_impl(self: &Self, input: &mut dyn io::Read, p: usize) -> BoxResult<i64> {
-        let numbers = self.numbers(input);
+        let numbers = self.numbers(input)?;
+        let n = self.find_invalid(&numbers, p)?;
+        Ok((0..numbers.len() - 1).flat_map(|i| numbers.as_slice()[i..].iter().scan((0, None, None), |s, &x| {
+            *s = (s.0 + x, Some(s.1.map_or(x, |m| min(x, m))), Some(s.2.map_or(x, |m| max(x, m))));
+            Some(*s)
+        }).skip(1).find(|(sum, _, _)| *sum >= n)).find(|(sum, _, _)| *sum == n)
+        .map(|(_, min, max)| min.unwrap() + max.unwrap()).unwrap())
+    }
+
+    #[allow(dead_code)]
+    fn part2_impl_naive(self: &Self, input: &mut dyn io::Read, p: usize) -> BoxResult<i64> {
+        let numbers = self.numbers(input)?;
         let n = self.find_invalid(&numbers, p)?;
         Ok((0..numbers.len() - 1).map(|i| {
             let mut s = 0;
